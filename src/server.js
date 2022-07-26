666f110df388390202f6cd0e4e11257573774398
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-26 23:03:08 +0900

    ✨ impl resolver

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ import { ApolloServer, gql } from "apollo-server";
 //define GraphQL schema
 const typeDefs = gql`
   type Query {
-    info: string!
+    info: String!
   }
 `;
+
+//define resolver
+// set value to typeDefs
+const resolvers = {
+  Query: {
+    info: () => "HackerNewsClone",
+  },
+};

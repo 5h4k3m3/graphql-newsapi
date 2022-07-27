e0529aedb88ef141cadff0fc0136dc2a330cdd1c
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-27 23:10:58 +0900

    ✨ connect graphql to db

--- src/server.js
+++ src/server.js
@@ -2,32 +2,29 @@ const { ApolloServer, gql } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
-const links = [
-  {
-    id: "link-0",
-    description: "Learn GraphQL",
-    url: "www.graphql-tutorial.com",
-  },
-];
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 //define resolver
 // set value to typeDefs
 const resolvers = {
   Query: {
     info: () => "HackerNewsClone",
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
 
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-      links.push(link);
-      return link;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
     },
   },
 };
@@ -35,6 +32,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`Start the server at ${url}`));

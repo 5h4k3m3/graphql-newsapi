ad74dd50310fd8a4fd753ce4549f97c1916153d7
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-27 22:50:35 +0900

    ✨ create table

---
+++ src/script.js
@@ -0,0 +1,24 @@
+// client library to access database
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "Learn GraphQL",
+      url: "www.graphql-tutorial.com",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    // close db connection
+    prisma.$disconnect;
+  });

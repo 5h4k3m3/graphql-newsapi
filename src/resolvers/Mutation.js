8a521a10ba9035a1bae0f9a30b4cb17672a777bb
SalmonFillet <sy4.l3ucas@gmail.com>
2022-08-01 21:31:02 +0900

    ✨ login resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,7 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+APP_SECRET = "GRAPHQL";
 
 async function signup(parent, args, context) {
   // password
@@ -10,4 +13,29 @@ async function signup(parent, args, context) {
       password,
     },
   });
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+}
+
+async function login(parent, args, context) {
+  const user = await context.prisma.user.findUnique({
+    where: { email: args.email },
+  });
+  if (!user) {
+    throw new Error("user does not exist.");
+  }
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error("password error");
+  }
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+  return {
+    token,
+    user,
+  };
 }
